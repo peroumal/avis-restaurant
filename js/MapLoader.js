60806da5aec6c3a893b096f49e234ecf3bfddb98
rayanperoumal <rayan.peroumal@gmail.com>
2018-03-21 09:53:07 -0400

    delete MapLoader.js

--- js/MapLoader.js
+++
@@ -1,9 +0,0 @@
-
-function loadMapApi(){
-  var element = document.getElementById("mapload");
-  var url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCug_AnkBdLsHSH8jpkBz7OswF_tGdbRWQ&callback=initMap";
-  element.setAttribute("async","");
-  element.setAttribute("defer","");
-  element.setAttribute("src",url);
-}
-loadMapApi();
