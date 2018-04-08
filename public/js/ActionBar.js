aea3a8d8ff72e5d97a0df4225c2cbc8de9479e6c
Rayan Peroumal <rayan.peroumal@gmail.com>
2018-04-08 18:15:09 -0400

    add untracked files on last commit

---
+++ public/js/ActionBar.js
@@ -0,0 +1,21 @@
+var ActionBar = {
+  isExpanded:true,
+  HOME:null,
+  get: function(name){
+    return document.getElementById("action-"+name);
+  },
+  set: function(title,back){
+      this.back = ActionBar.HOME;
+      this.get("title").textContent = "Liste des restaurants";
+      this.get("icon").setAttribute("src","assets/pic/back.svg");
+    if(title != null){
+      this.get("title").textContent = title;
+      this.get("icon").setAttribute("src","assets/pic/back.svg");
+      ActionBar.get("icon").addEventListener("click",function _func(e){
+          if(back!=undefined && back!=null) back();
+          e.target.setAttribute("src","assets/pic/search.svg");
+          e.target.removeEventListener("click",_func);
+      });
+    }
+  }
+};
---
+++ public/js/Launcher.js
@@ -0,0 +1,61 @@
+var mode = "near"
+
+$.ajax({
+    url: publicHtmlFolder+"assets/json/restaurants.json",
+    async: false,
+    dataType: "json",
+    success: function(data){
+      restaurants =[];
+      data.forEach(function(e){
+        restaurant = new Restaurant(e);
+        restaurants.push(restaurant);
+        //For offline test : appendRestaurant(restaurant);
+      });
+    }
+});
+
+
+function setRestaurantListDimension(){
+  var actionBarHeight = window.getComputedStyle(
+    document.getElementById("action-bar"))
+    .getPropertyValue("height")
+    .split("px")[0];
+
+    var actionBarWidth = window.getComputedStyle(
+      document.getElementById("action-bar"))
+      .getPropertyValue("width")
+      .split("px")[0];
+
+  if(actionBarWidth*2>=window.innerWidth){
+    document.getElementById("map").style.left = "0px";
+    document.getElementById("map").style.width = (window.innerWidth)+"px";
+  }else {
+    document.getElementById("map").style.left = actionBarWidth+"px";
+    document.getElementById("map").style.width = (window.innerWidth-actionBarWidth)+"px";
+  }
+  document.getElementById("map").style.height = (window.innerHeight)+"px";
+  document.getElementById("restaurant-list").style.height = (window.innerHeight-actionBarHeight)+"px";
+}
+
+window.addEventListener("resize", setRestaurantListDimension);
+window.addEventListener("load", setRestaurantListDimension);
+
+function displayMap(lat,lng){
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: lat, lng: lng}
+  });
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+      if(mode == "near") showRestaurants();
+  });
+}
+
+function initMap() {
+  if(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(function(pos){
+      displayMap(pos.coords.latitude, pos.coords.longitude);
+    },function(){
+      displayMap(restaurants[0].lat,restaurants[0].long);
+    });
+   else displayMap(restaurants[0].lat,restaurants[0].long);
+}
